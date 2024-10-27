8c9f0fa8aae11803a5d876743585d430f25f7c38
Joseph2412 <63360070+Joseph2412@users.noreply.github.com>
2024-10-27 13:06:34 +0100

    Traccia 1 Conclusa

---
+++ 2024-10-27 - TRACCIA 1/Javascript.js
@@ -0,0 +1,49 @@
+
+//Dichiarazione VAR. NUMERICA num1, num2
+
+
+let num1 = 30;
+let num2 = 40;
+
+//Visualizzazione tramite console.log
+
+console.log ( "Il valore della variabile num1 è " + num1 );
+
+console.log ("Il valore della variabile num2 è" + " " + num2 );
+
+
+//Dichiarazione VAR. Stringa di tipo STRINGA
+
+let stringa = "Prova Stringa";
+
+//Stampa a schermo tramite ALERT
+
+alert(stringa)
+
+
+//Nuovo valore di TIPO STRINGA a VAR. stringa
+
+stringa = "Seconda Prova Stringa";
+
+//Secondo ALERT con valore di stringa diverso
+
+alert(stringa);
+
+
+
+//dichiarazione CONSTANTE PIGRECO
+
+const PIGRECO = 3.14159265359;
+
+//Stampa a schermo tramite ALERT della CONST PIGRECO
+
+alert("Il valore di PRIGECO è " + PIGRECO);
+
+
+//Cambio Valore a CONST con conseguente ERRORE BLOCCANTE; Le CONST non possono variare valore.
+
+PIGRECO = 654351312;
+
+console.log(PIGRECO);
+
+ 
